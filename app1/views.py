eb47945f2aa8808837f3625ddc846f4a8983e888
Ramayanam-Balaji <balaji.r4454@gmail.com>
2023-06-23 13:53:01 +0530

    first commit

--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.db.models.functions import Length
 # Create your views here.
 from app1.models import *
 def display_topics(request):
@@ -8,6 +8,22 @@ def display_topics(request):
     return render(request,'display_topics.html',d)
 def display_Webpages(request):
     Webpages=Webpage.objects.all()
+    #Webpages=Webpage.objects.get(Webpage)
+    Webpages=Webpage.objects.filter(Name='virat')
+    Webpages=Webpage.objects.exclude(Name='virat')
+    Webpages=Webpage.objects.all().order_by('Name')
+    Webpages=Webpage.objects.all().order_by('-Name')
+    Webpages=Webpage.objects.all().order_by(Length('Name'))
+    Webpages=Webpage.objects.all()[1:4]
+    Webpages=Webpage.objects.all()[::-1]
+
+    
+    
+    
+    
+
+
+
     d1={'Webpages':Webpages}
     return render(request,'display_Webpages.html',d1)
 def display_AccessRecordss(request):
